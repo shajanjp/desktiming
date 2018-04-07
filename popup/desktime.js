2252221da264515ca355f11ad6264173b8a99dd0
Shajan Jacob <shajanjp@gmail.com>
2018-04-07 10:26:06 +0530

    lints code

--- popup/desktime.js
+++ popup/desktime.js
@@ -17,23 +17,23 @@ function TimeMachine(total_seconds) {
   };
 }
 
-function normalizeTo100(num_array){
-  var numbers = num_array;
-  var ratio = Math.max(...numbers) / 100;
-  return numbers.map(v => Math.round(v / ratio));
+function normalizeTo100(num_array) {
+  let numbers = num_array;
+  let ratio = Math.max(...numbers) / 100;
+  return numbers.map((v) => Math.round(v / ratio));
 }
 
 function onError(error) {
-  at.innerHTML = "Change API"
+  at.innerHTML = 'Change API';
 }
 
-function showForm(){
+function showForm() {
  APIform.style.display = 'block';
 }
 
-function getAPI_key(){
+function getAPI_key() {
   key_from_user = key_input.value;
-  storingAPI = browser.storage.local.set({ 'keey' : key_from_user });
+  storingAPI = browser.storage.local.set({'keey': key_from_user});
   storingAPI.then(() => {
   })
   .catch(onError);
@@ -42,32 +42,32 @@ function getAPI_key(){
 function alertContents() {
   if (httpRequest.readyState === XMLHttpRequest.DONE) {
     if (httpRequest.status === 200) {
-      var user = JSON.parse(httpRequest.responseText);
-      if(!user.error) { 
-       var normed_w = normalizeTo100([user.atWorkTime, 32400]);
-       var normed_d = normalizeTo100([user.desktimeTime, 28800]);
-       document.getElementById("work-time-graph").setAttribute('stroke-dasharray',  '' + normed_w[0] + ' ' + (100 - normed_w[0]));
-       document.getElementById("desk-time-graph").setAttribute('stroke-dasharray',  '' + normed_d[0] + ' ' + (100 - normed_d[0]));
-       var on_desk = new TimeMachine(user.desktimeTime);
-       var at_work = new TimeMachine(user.atWorkTime);
-       var on_desk_since = on_desk.humanize();  
-       var at_work_since = at_work.humanize();
+      let user = JSON.parse(httpRequest.responseText);
+      if (!user.error) {
+       let normed_w = normalizeTo100([user.atWorkTime, 32400]);
+       let normed_d = normalizeTo100([user.desktimeTime, 28800]);
+       document.getElementById('work-time-graph').setAttribute('stroke-dasharray', '' + normed_w[0] + ' ' + (100 - normed_w[0]));
+       document.getElementById('desk-time-graph').setAttribute('stroke-dasharray', '' + normed_d[0] + ' ' + (100 - normed_d[0]));
+       let on_desk = new TimeMachine(user.desktimeTime);
+       let at_work = new TimeMachine(user.atWorkTime);
+       let on_desk_since = on_desk.humanize();
+       let at_work_since = at_work.humanize();
 
-       dt.innerHTML = "<b>" + on_desk_since.hours + " : " + on_desk_since.minutes + "</b>";
-       at.innerHTML = "<b>" + at_work_since.hours + " : " + at_work_since.minutes + "</b>";
-     }
-     else
-       time_left.innerHTML = 'Invalid Key';
+       dt.innerHTML = '<b>' + on_desk_since.hours + ' : ' + on_desk_since.minutes + '</b>';
+       at.innerHTML = '<b>' + at_work_since.hours + ' : ' + at_work_since.minutes + '</b>';
+     } else {
+time_left.innerHTML = 'Invalid Key';
+}
    } else {
     alert('There was a problem with the request.');
   }
 }
 }
 
-if (window.XMLHttpRequest) { 
+if (window.XMLHttpRequest) {
   httpRequest = new XMLHttpRequest();
-} else if (window.ActiveXObject) { 
-  httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+} else if (window.ActiveXObject) {
+  httpRequest = new ActiveXObject('Microsoft.XMLHTTP');
 }
 
 httpRequest.onreadystatechange = alertContents;
@@ -75,14 +75,15 @@ httpRequest.onreadystatechange = alertContents;
 var gettingAPI = browser.storage.local.get(null);
 gettingAPI.then((result) => {
   API_keey = result['keey'];
-  if(API_keey == '' || API_keey == null || API_keey == undefined)
-    dt.innerHTML = "Change Key";
-  else
-    key_input.value = API_keey;
+  if (API_keey == '' || API_keey == null || API_keey == undefined) {
+dt.innerHTML = 'Change Key';
+} else {
+key_input.value = API_keey;
+}
   httpRequest.open('GET', 'https://desktime.com/api/v2/json/employee?apiKey=' + API_keey);
   httpRequest.send();
 })
 .catch(onError);
 
-dt.addEventListener("click", showForm);
-save_api.addEventListener("click", getAPI_key);
+dt.addEventListener('click', showForm);
+save_api.addEventListener('click', getAPI_key);
